80b110d0cbe057a1caaefc367637017c8577816d
Tom Dalling <tom@tomdalling.com>
2020-10-22 18:41:31 +1100

    Refactor, add tests, add comments

--- lib/ford_johnson.rb
+++ lib/ford_johnson.rb
@@ -11,27 +11,25 @@ module FordJohnson
 
     # Break the input array into pairs and stragglers. Odd-sized inputs will
     # have a single straggler, and even-sized inputs will have no stragglers.
-    pairs, stragglers = Pair.all_from(elements)
+    pairs = elements.each_slice(2).to_a
+    stragglers = (elements.size.odd? ? pairs.pop : [])
 
     # Sort within each pair.
-    pairs
-      .select { |p| greater_than?(p.left, p.right, comparator) }
-      .each(&:swap!)
+    pairs.each do |p|
+      p.reverse! if greater_than?(p.first, p.last, comparator)
+    end
 
-    # Sort between pairs, recursively. Pairs sort using their greater value.
+    # Sort between pairs recursively, using the greater (last) value.
     sorted_pairs = sort(pairs) do |left, right|
-      comparator.call(left.right, right.right)
+      comparator.call(left.last, right.last)
     end
 
     # Split into 'greater' and 'lesser' chains. The greater chain is also known
     # as "S" or the "main" chain. The lesser chain doesn't really have a name---
     # I've seen it called "prep" at least once. Stragglers go on the end of the
-    # lesser chain (this is important because it affects insertion order later).
-    greater_chain = sorted_pairs.map(&:right)
-    lesser_chain = sorted_pairs.map(&:left) + stragglers
-
-    # First insertion from lesser_chain is always at the front
-    greater_chain.unshift(lesser_chain.shift)
+    # lesser chain (this is important because it affects insertion order).
+    greater_chain = sorted_pairs.map(&:last)
+    lesser_chain = sorted_pairs.map(&:first) + stragglers
 
     # Insert all lesser_chain elements into greater_chain, in a special order,
     # using an optimised binary search
@@ -66,8 +64,16 @@ module FordJohnson
     # insertion idx by one for each following element. To avoid this increase,
     # elements within a group must be inserted in reverse order. This still
     # increases the max insertion idx for the next group, but this algorithm
-    # increases it in such a way that it always lands another value one less
-    # than a power of two.
+    # increases it in such a way that it always lands on another value that is
+    # one less than a power of two.
+    #
+    # There is a special case for the first insertion. It is guaranteed to be
+    # inserted at index zero, and so doesn't require a binary search. The first
+    # elemement of both the lesser and greater chains belong to the same pair.
+    # Because the greater chain is sorted, and the first element is the higher
+    # value of a pair, then the lower value of the pair must be lower than every
+    # other element in the greater chain. Returning a max insertion idx of zero
+    # short-circuits the whole binary search algorithm.
     #
     # The max insertion idxs form the series:
     #
@@ -80,28 +86,30 @@ module FordJohnson
     # Therefore, for the input [e0, e1, e2, ...], this method yields
     # [eX, max_idx] pairs like so:
     #
-    #     [e1,3] [e0,3] [e3,7] [e2,7] [e9,15] [e8,15] [e7,15] ... [e4,15] ...
-    #     |  group 1  | |  group 2  | |              group 3            | ...
+    #     [e0,0] [e2,3] [e1,3] [e4,7] [e3,7] [e10,15] [e9,15] ... [e5,15] ...
+    #            |  group 1  | |  group 2  | |          group 3         | ...
     #
     # As an interesting side note: the group sizes series happens to be the
     # difference between adjacent pairs in the Jacobsthal series:
     # https://oeis.org/A001045
     #
-    def each_insertion_for(elements_to_insert)
-      group_start_idx = 0
+    def each_insertion_for(lesser_chain)
+      yield [lesser_chain.first, 0]
+
+      group_start_idx = 1 # skip over that first inserted element
       previous_group_size = 0
       power = 1
 
-      while group_start_idx < elements_to_insert.size
+      while group_start_idx < lesser_chain.size
         group_size = 2**power - previous_group_size
         group_last_idx = [
           group_start_idx + group_size - 1,
-          elements_to_insert.size - 1,
-        ].min # idx must not go off the end of elements_to_insert
+          lesser_chain.size - 1,
+        ].min # idx must not go off the end of lesser_chain
         max_insertion_idx = 2**(power+1) - 1
 
         group_last_idx.downto(group_start_idx).each do |idx|
-          yield [elements_to_insert[idx], max_insertion_idx]
+          yield [lesser_chain[idx], max_insertion_idx]
         end
 
         group_start_idx += group_size
@@ -110,8 +118,8 @@ module FordJohnson
       end
     end
 
-    # Group size determines max_idx, restricting the search range.
-    # This is the secret sauce of the whole algorithm.
+    # the search range is restricted by the max_idx parameter, which is how this
+    # algorithm reduces the number of comparisons so well.
     def binary_insert_idx(new_element, sorted_elements, max_idx, comparator)
       min_idx = 0
       max_idx = [max_idx, sorted_elements.size].min # can't go past end of sorted_elements
@@ -122,56 +130,11 @@ module FordJohnson
           # must be somewhere after middle_idx (right branch)
           min_idx = middle_idx + 1
         else
-          # must be middle_idx or earler (left branch)
+          # must be middle_idx or earlier (left branch)
           max_idx = middle_idx
         end
       end
 
       min_idx
-    end
-
-    def clamp_idx(idx, array)
-      if idx < array.size
-        idx
-      else
-        array.size - 1
-      end
-    end
-
-    class Pair
-      include Comparable
-
-      attr_reader :left, :right
-
-      def self.all_from(elements)
-        doublets = elements.each_slice(2).to_a
-        stragglers = (elements.size.odd? ? doublets.pop : [])
-        pairs = doublets.map { |left, right| new(left, right) }
-        [pairs, stragglers]
-      end
-
-      def initialize(left, right)
-        @left = left
-        @right = right
-      end
-
-      def to_a
-        [left, right]
-      end
-
-      def inspect
-        "#<Pair #{left.inspect} #{right.inspect}>"
-      end
-
-      def to_s
-        inspect
-      end
-
-      def swap!
-        tmp = @left
-        @left = @right
-        @right = tmp
-        nil
-      end
     end
 end
--- spec/unit/ford_johnson_spec.rb
+++ spec/unit/ford_johnson_spec.rb
@@ -1,10 +1,11 @@
 RSpec.describe FordJohnson do
   it 'sorts non-destructively' do
-    elements = [6,2,4,9].freeze
+    elements = [6,2,4,9]
 
     result = subject.sort(elements)
 
     expect(result).to eq([2,4,6,9])
+    expect(elements).to eq([6,2,4,9])
   end
 
   it 'takes a custom comparator block like Array#sort does' do
@@ -25,49 +26,51 @@ RSpec.describe FordJohnson do
     end
   end
 
-  # These are the optimal values (can't get any lower) according to
-  # information theory and some exhaustive searching.
+  # These are mostly optimal values (can't get any lower).
   # Source: https://github.com/decidedlyso/merge-insertion-sort
   describe 'minimises comparisons', order: :defined do
     EXHAUSTIVE_TESTING_UNTIL = 9
     OPTIMAL_MIN_COMPARISONS = {
-      0 => 0,
-      1 => 0,
-      2 => 1,
-      3 => 3,
-      4 => 5,
-      5 => 7,
-      6 => 10,
-      7 => 13,
-      8 => 16,
-      9 => 19,
-      10 => 22,
-      11 => 26,
-      12 => 30,
-      13 => 34,
-      14 => 38,
-      15 => 42,
+      0 => 0, # optimal by information theory
+      1 => 0, # optimal by information theory
+      2 => 1, # optimal by information theory
+      3 => 3, # optimal by information theory
+      4 => 5, # optimal by information theory
+      5 => 7, # optimal by information theory
+      6 => 10, # optimal by information theory
+      7 => 13, # optimal by information theory
+      8 => 16, # optimal by information theory
+      9 => 19, # optimal by information theory
+      10 => 22, # optimal by information theory
+      11 => 26, # optimal by information theory
+      12 => 30, # optimal by exhaustive search
+      13 => 34, # optimal by exhaustive search
+      14 => 38, # optimal by exhaustive search
+      15 => 42, # optimal by exhaustive search
+      16 => 46,
+      17 => 50,
+      18 => 54,
+      19 => 58,
+      20 => 62, # optimal by information theory
+      21 => 66, # optimal by information theory
+      22 => 71, # optimal by exhaustive search
     }
 
-    # exhaustively test all permutations of an n-sized array,
-    # up until n == EXHAUSTIVE_TESTING_UNTIL. Beyond that it just takes too
-    # long.
-    (0...EXHAUSTIVE_TESTING_UNTIL).each do |n|
-      optimal_minimum_comparisons = OPTIMAL_MIN_COMPARISONS.fetch(n)
+    # For small n's, exhaustively test all permutations of an n-sized array.
+    # For large n's that take too long to be tested exhaustively, switch to
+    # randomized testing.
+    exhaustives, randoms = OPTIMAL_MIN_COMPARISONS.partition { |n,_| n < 9 }
 
-      specify "when n == #{n}, with worst case #{optimal_minimum_comparisons} comparisons (exhaustive)" do
+    exhaustives.each do |n, optimal_minimum_comparisons|
+      specify "for n == #{n}, worst case #{optimal_minimum_comparisons} comparisons (exhaustive)" do
         1.upto(n).to_a.permutation do |input|
           expect(num_comparisons_to_sort(input)).to be <= optimal_minimum_comparisons
         end
       end
     end
 
-    # Randomised testing for n >= EXHAUSTIVE_TESTING_UNTIL. Just tries a bunch
-    # of random n-sized arrays.
-    (EXHAUSTIVE_TESTING_UNTIL..OPTIMAL_MIN_COMPARISONS.keys.max).each do |n|
-      optimal_minimum_comparisons = OPTIMAL_MIN_COMPARISONS.fetch(n)
-
-      specify "when n == #{n}, with worst case #{optimal_minimum_comparisons} comparisons (randomised)" do
+    randoms.each do |n, optimal_minimum_comparisons|
+      specify "for n == #{n}, worst case #{optimal_minimum_comparisons} comparisons (randomised)" do
         input = Array.new(n) { rand(1..99) }
         10_000.times do
           input.shuffle!
